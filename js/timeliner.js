ded800dba812bc550063648d7945ba257a75e7b0
Tarek Anandan <tarek@technotarek.com>
2013-01-25 00:06:32 -0500

    Added instantiation options and resolved issues related to event state (open/close) awareness

--- js/timeliner.js
+++ js/timeliner.js
@@ -1,6 +1,6 @@
 /*
 * Timeliner.js
-* @version		1.0
+* @version		1.2
 * @copyright	Tarek Anandan (http://www.technotarek.com)
 */
 ;(function($) {
@@ -10,9 +10,12 @@
 	// default plugin settings
 	settings = jQuery.extend({
 		timelineContainer: '#timelineContainer', // value: selector of the main element holding the timeline's content, default to #timelineContainer
-		startState: 'closed', // value: closed | open, default to closed; determines whether the timeline is initially collapsed or fully expanded
-		startOpen: '', // value: selector ID of single timelineEvent, default to empty; determines the minor event that you want to display open by default on page load
-		baseSpeed: 200 // value: any integer, default to 200; determines the base speed, some animations are a multiple (4x) of the base speed
+		startState: 'closed', // value: closed | open, default to closed; sets whether the timeline is initially collapsed or fully expanded
+		startOpen: '', // value: selector ID of single timelineEvent, default to empty; sets the minor event that you want to display open by default on page load
+		baseSpeed: 200, // value: numeric, default to 200; sets the base speed for animation of the event marker
+		speed: 4, // value: numeric, defalut to 4; a multiplier applied to the base speed that sets the speed at which an event's conents are displayed and hidden
+		fontOpen: '1.2em', // value: any valid CSS font-size value, defaults to 1em; sets the font size of an event after it is opened
+		fontClosed: '1em' // value: any valid CSS font-size value, defaults to 1em; sets the font size of an event after it is closed
 	}, options);
 
 		$(document).ready(function() {
@@ -26,61 +29,77 @@
 				$(settings.startOpen).parent(".timelineMinor").find("dt a")
 					.removeClass('closed')
 					.addClass('open')
-					.animate({ fontSize: "1.2em" }, settings.baseSpeed);
-				$(settings.startOpen).show(4*settings.baseSpeed);
+					.animate({ fontSize: settings.fontOpen }, settings.baseSpeed);
+				$(settings.startOpen).show(settings.speed*settings.baseSpeed);
 
 			}else{
 				$(".timelineMinor dt, .timelineMinor dt a")
 					.addClass('open')
-					.css("fontSize", "1.2em");
+					.css("fontSize", settings.fontOpen);
 				$(".timelineEvent").show();
 			}
 
-			// Single Event
-			$(".timelineMinor dt").toggle(function(){
+			// Minor Event Click
+			$(".timelineMinor dt").click(function(){
 
 				var currentId = $(this).attr('id');
 
-				// open Event
-				$("a",this)
-					.removeClass('closed')
-					.addClass('open')
-					.animate({ fontSize: "1.2em" }, settings.baseSpeed);
+				// if the event is currently open
+				if($(this).find('a').is('.open'))
+				{
 
-				$("#"+currentId+"EX").show(4*settings.baseSpeed);
+					// close the Event
+					$("a",this)
+						.animate({ fontSize: settings.fontClosed }, 0)
+						.removeClass('open')
+						.addClass('closed');
 
-			},function()
-			{
-				var currentId = $(this).attr('id');
+					$("#"+currentId+"EX").hide(settings.speed*settings.baseSpeed);
 
-				// close Event
-				$("a",this)
-					.animate({ fontSize: "1.0em" }, 0)
-					.removeClass('open')
-					.addClass('closed');
+				} else{ // if the event is currently closed
 
-				$("#"+currentId+"EX").hide(4*settings.baseSpeed);
+					// open the Event
+					$("a", this)
+						.removeClass('closed')
+						.addClass('open')
+						.animate({ fontSize: settings.fontOpen }, settings.baseSpeed);
+
+					$("#"+currentId+"EX").show(settings.speed*settings.baseSpeed);
+
+				}
 
 			});
 
-			// Single Major Marker
-			$(".timelineMajorMarker").toggle(function()
+			// Major Marker Click
+			$(".timelineMajorMarker").click(function()
 			{
-				// reset all animations
-				$(this).parents(".timelineMajor").find("dt a","dl.timelineMinor")
-					.animate({ fontSize: "1.2em" }, settings.baseSpeed)
-					.removeClass('closed')
-					.addClass('open');
-				$(this).parents(".timelineMajor").find(".timelineEvent").show(4*settings.baseSpeed);
 
-			},function()
-			{
-				// reset all animations
-				$(this).parents(".timelineMajor").find("dl.timelineMinor a")
-					.animate({ fontSize: "1.0em" }, settings.baseSpeed)
-					.removeClass('open')
-					.addClass('closed');
-				$(this).parents(".timelineMajor").find(".timelineEvent").hide(4*settings.baseSpeed);
+				// number of minor events under this major event
+				var numEvents = $(this).parents(".timelineMajor").find(".timelineMinor").length;
+
+				// number of minor events already open
+				var numOpen = $(this).parents(".timelineMajor").find('.open').length;
+
+				if(numEvents > numOpen )
+				{
+
+					// reset all animations
+					$(this).parents(".timelineMajor").find("dt a","dl.timelineMinor")
+						.animate({ fontSize: settings.fontOpen }, settings.baseSpeed)
+						.removeClass('closed')
+						.addClass('open');
+					$(this).parents(".timelineMajor").find(".timelineEvent").show(settings.speed*settings.baseSpeed);
+
+				} else{
+
+					// reset all animations
+					$(this).parents(".timelineMajor").find("dl.timelineMinor a")
+						.animate({ fontSize: settings.fontClosed }, settings.baseSpeed)
+						.removeClass('open')
+						.addClass('closed');
+					$(this).parents(".timelineMajor").find(".timelineEvent").hide(settings.speed*settings.baseSpeed);
+
+				}
 			});
 
 			// All Markers/Events
@@ -88,20 +107,20 @@
 			{
 				// reset all animations
 				$(this).parents(settings.timelineContainer).find("dt a","dl.timelineMinor")
-					.animate({ fontSize: "1.2em" }, settings.baseSpeed)
+					.animate({ fontSize: settings.fontOpen }, settings.baseSpeed)
 					.removeClass('closed')
 					.addClass('open');
-				$(this).parents(settings.timelineContainer).find(".timelineEvent").show(4*settings.baseSpeed);
+				$(this).parents(settings.timelineContainer).find(".timelineEvent").show(settings.speed*settings.baseSpeed);
 				$(this).html("- collapse all");
 
 			},function()
 			{
 				// reset all animations
 				$(this).parents(settings.timelineContainer).find("dl.timelineMinor a")
-					.animate({ fontSize: "1.0em" }, settings.baseSpeed)
+					.animate({ fontSize: settings.fontClosed }, settings.baseSpeed)
 					.removeClass('open')
 					.addClass('closed');
-				$(this).parents(settings.timelineContainer).find(".timelineEvent").hide(4*settings.baseSpeed);
+				$(this).parents(settings.timelineContainer).find(".timelineEvent").hide(settings.speed*settings.baseSpeed);
 				$(this).html("+ expand all");
 			});
 		});
